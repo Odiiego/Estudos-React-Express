0818aa0dd83c74239f7f173b550d7f13de6ffcd8
Odiiego <odiegopaula@gmail.com>
2022-12-22 02:45:29 -0300

    Instalei algumas bibliotecas.

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css';
+
+function App() {
+
+  const [todos, setTodos] = useState(null)
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div className="App">
+      x
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const { v4: uuidv4 } = require('uuid')
+const app = express()
+
+app.use(express.json({ extended: false }));
+
+const todos = [
+    {
+        message:"Wash Car...",
+        id: 1
+    },
+    {
+        message: "Go for run...",
+        id: 2
+    },
+    {
+        message: "Cook dinner...",
+        id: 3
+    }
+]
+
+app.get("/", (req, res) => {
+    res.status(200).json(todos);
+});
+
+app.post("/", (req, res) => {
+    const newTodo = {
+        message: req.body.message,
+        id: uuidv4()
+    }
+
+    todos.push(newTodo)
+    res.status(201).json(todos)
+})
+
+const PORT = 5001
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
+})
