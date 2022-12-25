e6e356b0bbbe395e187b4a676ff56659329b4b3b
Odiiego <odiegopaula@gmail.com>
2022-12-25 01:28:20 -0300

    Terminei a primeira parte do tutorial.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Todos from './components/Todos';
+import Preloader from './components/Preloader';
+import Header from './components/Header';
+import TodoInput from './components/TodoInput';
 import './App.css';
 
 function App() {
@@ -9,16 +12,25 @@ function App() {
 
   useEffect(() => {
     const getTodos = async () => {
-      const res = await axios.get("http://localhost:5001")
+      const res = await axios.get("http://localhost:5001");
       setTodos(res.data);
-    }
+    };
 
-    getTodos()
-  }, [])
+    getTodos();
+  }, []);
+
+  const createTodo = async (text) => {
+    const res = await axios.post("http://localhost:5001", {message: text})
+    setTodos(res.data)
+  }
 
   return (
     <div className="App">
-      <Todos todos={todos} />
+      <div className="container">
+        <Header />
+        <TodoInput createTodo={createTodo} />
+        {todos ? <Todos todos={todos} /> : <Preloader />}
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="heading">
+            <h1>React Todo app</h1>
+            <p>React and Express Course</p>
+        </div>
+    )
+}
+
+export default Header
